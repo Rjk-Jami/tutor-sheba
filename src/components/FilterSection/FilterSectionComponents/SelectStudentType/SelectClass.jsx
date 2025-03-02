fda4d2f53566397ff7609458e13841764d370d59
Rjk-Jami <rjkhan.1221@gmail.com>
2025-03-02 15:34:33 +0600

    added class of mediums

--- src/components/FilterSection/FilterSectionComponents/SelectStudentType/SelectClass.jsx
+++ src/components/FilterSection/FilterSectionComponents/SelectStudentType/SelectClass.jsx
@@ -2,19 +2,28 @@
 import React, { useState } from 'react'
 import FilterHeader from '../../FilterHeader';
 import FormSelect from '@/components/FormSelect/FormSelect';
+import { useDispatch, useSelector } from 'react-redux';
+import { useGetClassesQuery } from '../../../../../redux/api/rootApi';
+import { setClassOfMedium } from '../../../../../redux/filter/commonFilterSlice';
 
 const SelectClass = () => {
-    const classOptions = ["All", "class 1", "class 2"];
-        
-          const [selectedClass, setSelectedClass] = useState("All");
-          console.log(selectedClass);
+  const selectedMedium = useSelector((state) => state.filters.medium);
+
+ // RTK Query for Location for selected district
+  const { data: classes = [], isLoading: classesLoading } =
+    useGetClassesQuery(selectedMedium, {
+      skip: !selectedMedium && selectedMedium !== "All", // Skip query if no district is selected
+    });
+    const classOfMedium = useSelector((state) => state.filters.classOfMedium);
+    const dispatch = useDispatch();
+
   return (
     <div>
     <FilterHeader>Select Class</FilterHeader>
     <FormSelect
-      options={classOptions}
-      selectedValue={selectedClass}
-      onChange={setSelectedClass}
+      options={classes}
+      selectedValue={classOfMedium}
+      onChange={(e)=>dispatch(setClassOfMedium(e))}
     />
   </div>
   )
--- src/components/FormSelect/FormSelect.jsx
+++ src/components/FormSelect/FormSelect.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const FormSelect = ({ options, selectedValue, onChange }) => {
   return (
@@ -7,15 +7,15 @@ const FormSelect = ({ options, selectedValue, onChange }) => {
       onChange={(e) => onChange(e.target.value)}
       className="select select-bordered w-full max-full mt-4 px-5 py-1.5 font-normal border rounded-md text-base focus:border-2 border-gray-300 focus:outline-none"
     >
-      <option  defaultValue={"all"}>All</option>
-       
+      <option defaultValue={"all"}>All</option>
+
       {options.map((option, index) => (
         <option key={index} value={option._id}>
           {option.name}
         </option>
       ))}
     </select>
-  )
-}
+  );
+};
 
-export default FormSelect
+export default FormSelect;
